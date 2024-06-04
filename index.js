52f5504ebb632de97e7b1352df9fed0d4759d7f0
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-06-04 21:38:19 +0200

    updated the html file

---
+++ index.js
@@ -0,0 +1,33 @@
+let score = 0;
+
+function addOne(event) {
+  event.preventDefault();
+  let homeScore = document.querySelector("#home-score");
+  score = score + 1;
+  homeScore.innerHTML = score;
+}
+
+let buttonOne = document.querySelector("#home-1");
+buttonOne.addEventListener("click", addOne);
+//first add function for the home
+
+function addTwo(event) {
+  event.preventDefault();
+  let homeScoreTwo = document.querySelector("#home-score");
+  score = score + 2;
+  homeScoreTwo.innerHTML = score;
+}
+
+let buttonTwo = document.querySelector("#home-2");
+buttonTwo.addEventListener("click", addTwo);
+//second function
+
+function addThree(event) {
+  event.preventDefault();
+  let homeScoreThree = document.querySelector("#home-score");
+  score = score + 3;
+  homeScoreThree.innerHTML = score;
+}
+
+let buttonThree = document.querySelector("#home-3");
+buttonThree.addEventListener("click", addThree);
